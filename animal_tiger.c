1238b00f90c7516306fa05c4b9ac6f8a415b6bc3
Layerex <layerex@dismail.de>
2022-12-05 18:26:25 +0300

    Add tiger destructor

--- animal_tiger.c
+++ animal_tiger.c
@@ -16,11 +16,26 @@ static void animal_tiger_cat_say_meow(AnimalCat *parent)
     printf("Tiger %s says: GRRR!\n", animal_cat_get_name(parent));
 }
 
+static void animal_tiger_finalize(GObject *object)
+{
+    // Can't get name property with animal_cat_get_name(ANIMAL_CAT(object)) anymore,
+    // because when finalize have been entered, object can't be referenced anymore
+    // (referencing is required to get property)
+    printf("Tiger vanished.\n");
+
+    G_OBJECT_CLASS(animal_tiger_parent_class)->finalize(object);
+}
+
 static void animal_tiger_class_init(AnimalTigerClass *klass)
 {
     printf("First instance of AnimalTiger was created.\n");
+
     AnimalCatClass *parent_class = ANIMAL_CAT_CLASS(klass);
     parent_class->say_meow = animal_tiger_cat_say_meow;
+
+    GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+    object_class->finalize = animal_tiger_finalize;
 }
 
 static void animal_tiger_init(AnimalTiger *self)
